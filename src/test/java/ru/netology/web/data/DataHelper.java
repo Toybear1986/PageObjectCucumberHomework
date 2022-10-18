b953c0cc7f04a437b2a4e98741def708422f0723
Василий Скоробогатов <vaskorobogatov@yandex.ru>
2022-10-18 17:57:28 +0300

    Add Page Objects and tests for transactions

---
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -0,0 +1,47 @@
+package ru.netology.web.data;
+
+import lombok.Value;
+import ru.netology.web.page.CheckBalance;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class DataHelper {
+  private DataHelper() {
+  }
+
+  public static String card0001 = "5559 0000 0000 0001";
+  public static String card0002 = "5559 0000 0000 0002";
+//  public static double randomValue = ThreadLocalRandom.current().nextInt(1, 1000 + 1); //закоментил, т.к. переводы работают не правильно
+
+  @Value
+  public static class AuthInfo {
+    String login;
+    String password;
+  }
+
+  public static AuthInfo getAuthInfo() {
+    return new AuthInfo("vasya", "qwerty123");
+  }
+
+
+/*  public static AuthInfo getOtherAuthInfo(AuthInfo original) {
+    return new AuthInfo("petya", "123qwerty");
+  }*/
+
+  @Value
+  public static class VerificationCode {
+    String code;
+  }
+
+  public static VerificationCode getVerificationCodeFor() {
+    return new VerificationCode("12345");
+  }
+
+  public static int parseIntFromString(String card) {
+    CheckBalance.checkBalance(card);
+    int index1 = card.indexOf(": \n") + 3;
+    int index2 = card.indexOf("\n р.");
+    return Integer.parseInt(card, index1, index2, 10);
+  }
+}
---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,85 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+import ru.netology.web.page.MoneyTransfer;
+
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.web.data.DataHelper.*;
+import static ru.netology.web.page.CheckBalance.checkBalance;
+
+class MoneyTransferTest {
+
+  @BeforeEach
+  void setup() {
+    open("http://localhost:9999", LoginPage.class);
+  }
+
+  @AfterEach
+  void tearDown() {
+    clearBrowserCookies();
+    closeWebDriver();
+  }
+
+  @Test
+  void shouldTransferMoneyBetweenOwnCardsCheckTargetCard() {
+    var loginPage = new LoginPage();
+    var authInfo = DataHelper.getAuthInfo();
+    var verificationPage = loginPage.validLogin(authInfo);
+    var verificationCode = DataHelper.getVerificationCodeFor();
+    verificationPage.validVerify(verificationCode);
+    int checkBalanceTo = checkBalance(card0001);
+    var value = 100;
+    MoneyTransfer.rechargeCard(card0001, value);
+    System.out.println(checkBalance(card0001) - (checkBalanceTo + value));
+    assertEquals(checkBalanceTo + value, checkBalance(card0001));
+  }
+
+
+  @Test
+  void shouldTransferMoneyBetweenOwnCardsWithKopecksCheckTargetCard() {
+    var loginPage = new LoginPage();
+    var authInfo = DataHelper.getAuthInfo();
+    var verificationPage = loginPage.validLogin(authInfo);
+    var verificationCode = DataHelper.getVerificationCodeFor();
+    verificationPage.validVerify(verificationCode);
+    int checkBalanceTo = checkBalance(card0002);
+    var value = 100;
+    MoneyTransfer.rechargeCard(card0002, value);
+    System.out.println(checkBalance(card0002) - (checkBalanceTo + value));
+    assertEquals(checkBalanceTo + value, checkBalance(card0002));
+  }
+
+  @Test
+  void shouldTransferMoneyBetweenOwnCardsCheckSourceCard() {
+    var loginPage = new LoginPage();
+    var authInfo = DataHelper.getAuthInfo();
+    var verificationPage = loginPage.validLogin(authInfo);
+    var verificationCode = DataHelper.getVerificationCodeFor();
+    verificationPage.validVerify(verificationCode);
+    int checkBalanceFrom = checkBalance(card0002);
+    var value = 100;
+    MoneyTransfer.rechargeCard(card0001, value);
+    System.out.println((checkBalanceFrom + value) - checkBalance(card0002));
+    assertEquals(checkBalanceFrom + value, checkBalance(card0001));
+  }
+
+
+  @Test
+  void shouldTransferMoneyBetweenOwnCardsWithKopecksCheckSourceCard() {
+    var loginPage = new LoginPage();
+    var authInfo = DataHelper.getAuthInfo();
+    var verificationPage = loginPage.validLogin(authInfo);
+    var verificationCode = DataHelper.getVerificationCodeFor();
+    verificationPage.validVerify(verificationCode);
+    int checkBalanceFrom = checkBalance(card0001);
+    var value = 100;
+    MoneyTransfer.rechargeCard(card0002, value);
+    System.out.println((checkBalanceFrom + value) - checkBalance(card0001));
+    assertEquals(checkBalanceFrom + value, checkBalance(card0001));
+  }
+}
